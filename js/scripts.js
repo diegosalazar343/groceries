e2ec4e8b20ac95111bccc72a7655e8c1147f15e0
diegosalazar343 <diegosalazar343@gmail.com>
2020-10-26 15:47:06 -0700

    figure out how to alphabetize list
    Co-authored-by: Erica Woodmansee <ewoodmansee@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,13 @@ flavors.forEach(function (element) {
 /*var groceries = [x, y, z];
 groceries.sort();*/
 
+/* var arr = [[1,2], [3,4], [5,6]];
+ for (var i=0; i < arr.length; i++) {
+  for (var j=0; j < arr[i].length; j++) {
+    console.log(arr[i][j]);
+  }
+}*/
+
 $(document).ready(function () {
   $("#groceryList").submit(function(event) {
     event.preventDefault();
@@ -18,9 +25,11 @@ $(document).ready(function () {
 
     const groceries = [p1, p2, p3, p4, p5];
     groceries.forEach(function(element) {
+      groceries.sort();
+      document.getElementById("list").innerHTML = groceries;
+    })
       $('#list').append(element + " ");
     });
 
-  });
   $('#result').show();
-});
+  });
